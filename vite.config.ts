2284bc4a967bb4a835587b2e48093947b500351d
Akshay <akshaykumar059004@gmail.com>
2025-07-14 00:19:51 +0530

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html', // Main entry point for SPA
+    },
   },
+  base: '/portfolio/', // Change to your repo name
 });
