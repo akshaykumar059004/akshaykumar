f3a9358ab4119220331a4de2bc0ddfa665808d82
Akshay <akshaykumar059004@gmail.com>
2025-07-17 15:55:24 +0530

    Deployment fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: 'index.html', // Main entry point for SPA
-    },
-  },
-  base: '/portfolio/', // Change to your repo name
+  base: '/portfolio/'// Change to your repo name
 });
